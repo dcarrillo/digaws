2bc83a94d60104cdc7048be19676fe7d5b435cd3
Daniel Carrillo <daniel.carrillo@gmail.com>
2020-12-25 21:36:41 +0100

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,100 @@
 # digaws
+
+The digaws lookup tool displays information for a given IP address (v4 o v6) or a CIDR, sourced from the AWS official IP ranges.
+In order to save bandwidth and time this tool requests the [AWS IP ranges](https://ip-ranges.amazonaws.com/ip-ranges.json) and keeps
+a cached version until a new version is published.
+
+## Install
+
+```bash
+pip install digaws
+```
+
+## Usage
+
+```bash
+usage: digaws [-h] [--output <plain|json>] [--debug] <ip address|cidr> [<ip address|cidr> ...]
+
+Look up canonical information for AWS IP addresses and networks
+
+positional arguments:
+  <ip address|cidr>     CIDR or IP (v4 or v6) to look up
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --output <plain|json>
+                        Formatting style for command output, by default plain
+  --debug               Enable debug
+```
+
+## Examples
+
+- look up an IPv4 address
+
+```bash
+~ » digaws 52.218.97.130
+
+Prefix: 52.218.0.0/17
+Region: eu-west-1
+Service: AMAZON
+Network border group: eu-west-1
+
+Prefix: 52.218.0.0/17
+Region: eu-west-1
+Service: S3
+Network border group: eu-west-1
+```
+
+- look up an IPv6 address
+
+```bash
+~ » digaws 2600:1f1e:fff:f810:a29b:cb50:2812:e2dc
+
+IPv6 Prefix: 2600:1f1e::/36
+Region: sa-east-1
+Service: AMAZON
+Network border group: sa-east-1
+
+IPv6 Prefix: 2600:1f1e:fff:f800::/53
+Region: sa-east-1
+Service: ROUTE53_HEALTHCHECKS
+Network border group: sa-east-1
+
+IPv6 Prefix: 2600:1f1e::/36
+Region: sa-east-1
+Service: EC2
+Network border group: sa-east-1
+```
+
+- look up several addresses and print output as json
+
+```bash
+~ » digaws 2600:1f14::/36 13.224.119.88 --output json
+
+[
+  {
+    "ipv6_prefix": "2600:1f14::/35",
+    "region": "us-west-2",
+    "service": "AMAZON",
+    "network_border_group": "us-west-2"
+  },
+  {
+    "ipv6_prefix": "2600:1f14::/35",
+    "region": "us-west-2",
+    "service": "EC2",
+    "network_border_group": "us-west-2"
+  },
+  {
+    "ip_prefix": "13.224.0.0/14",
+    "region": "GLOBAL",
+    "service": "AMAZON",
+    "network_border_group": "GLOBAL"
+  },
+  {
+    "ip_prefix": "13.224.0.0/14",
+    "region": "GLOBAL",
+    "service": "CLOUDFRONT",
+    "network_border_group": "GLOBAL"
+  }
+]
+```
---
+++ tests/test_dig_aws.py
@@ -0,0 +1,77 @@
+import json
+import sys
+
+import digaws.digaws as digaws
+from digaws import __description__, __version__
+
+import pytest
+
+import tests
+
+
+@pytest.fixture
+def test_dig():
+    return digaws.DigAWS(json.loads(tests.AWS_IP_RANGES))
+
+
+def test_cli(capsys):
+    sys.argv = ['digaws', '-h']
+    try:
+        digaws.main()
+    except SystemExit as e:
+        out, _ = capsys.readouterr()
+        assert __description__ in out
+        assert e.code == 0
+
+
+def test_cli_version(capsys, mocker):
+    sys.argv = ['digaws', '--version']
+    try:
+        digaws.main()
+    except SystemExit as e:
+        out, _ = capsys.readouterr()
+        assert out == f'digaws {__version__}\n'
+        assert e.code == 0
+
+
+def test_cli_invocation(capsys, mocker):
+    sys.argv = ['digaws', '52.94.76.0/22', '2600:1f14:fff:f810:a1c1:f507:a2d1:2dd8',
+                '--output', 'json']
+    mocker.patch('digaws.digaws.get_aws_ip_ranges', return_value=json.loads(tests.AWS_IP_RANGES))
+    digaws.main()
+    out, _ = capsys.readouterr()
+
+    assert out == tests.RESPONSE_JSON_JOINED_PRINT
+
+
+def test_dig_aws_construct(test_dig):
+    assert test_dig.ip_prefixes == tests.AWS_IPV4_RANGES_OBJ
+    assert test_dig.ipv6_prefixes == tests.AWS_IPV6_RANGES_OBJ
+
+
+def test_lookup(test_dig):
+    assert str(test_dig._lookup_data('52.94.76.1')[0]['ip_prefix']) == '52.94.76.0/22'
+    assert str(test_dig._lookup_data('52.94.76.0/24')[0]['ip_prefix']) == '52.94.76.0/22'
+
+    input = '2600:1f14:fff:f810:a1c1:f507:a2d1:2dd8'
+    assert str(test_dig._lookup_data(input)[0]['ipv6_prefix']) == '2600:1f14:fff:f800::/53'
+    assert str(test_dig._lookup_data(input)[1]['ipv6_prefix']) == '2600:1f14::/35'
+    assert str(test_dig._lookup_data('2600:1f14::/36')[0]['ipv6_prefix']) == '2600:1f14::/35'
+
+    with pytest.raises(ValueError) as e:
+        test_dig.lookup('what are you talking about')
+        assert e.startswith('Wrong IP or CIDR format')
+
+
+def test_response_plain_print(test_dig, capsys):
+    test_dig.lookup('52.94.76.0/22').plain_print()
+    out, _ = capsys.readouterr()
+
+    assert out == tests.RESPONSE_PLAIN_PRINT
+
+
+def test_response_json_print(test_dig, capsys):
+    test_dig.lookup('2600:1f14:fff:f810:a1c1:f507:a2d1:2dd8').json_print()
+    out, _ = capsys.readouterr()
+
+    assert out == tests.RESPONSE_JSON_PRINT
